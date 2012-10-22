aeddfebfdea6441135e66c2919a1259ed83dc76e
Florian Biermann <fbie@itu.dk>
2012-10-22 17:26:20 +0200

    add describing comments and clean up

--- dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/environment/IEnvironment.java
+++ dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/environment/IEnvironment.java
@@ -1,18 +1,28 @@
 package dk.itu.ecdar.text.generator.environment;
 
-import java.util.AbstractMap;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import dk.itu.ecdar.text.generator.framework.*;
 
+/**
+ * Base class for environment implementations.
+ * 
+ * Use this to test the implemented ECDAR specifications.
+ * A text file provided can specify the input the environment
+ * receives while executing.
+ */
 public abstract class IEnvironment {
 	
+	/**
+	 * This thread class executes the controller separately 
+	 */
 	public class ControllerThread extends Thread{
 		IController controller;
 		
 		public ControllerThread(IController c) {
+			super();
 			controller = c;
 		}
 		
@@ -23,30 +33,53 @@ public abstract class IEnvironment {
 	
 	IController controller;
 	AutomatonTimer timer;
-    ArrayList<AbstractMap.SimpleEntry<Long, IInput.IInputEnum>> inputs;
+    ArrayList<SimpleEntry<Long, IInput.IInputEnum>> inputs;
 	
+    /**
+     * Needs to be implemented for every implemented ECDAR specification.
+     */
     public abstract void generateController();
     
 	public IEnvironment() {
 		generateController();
 		timer = new AutomatonTimer();
-		inputs = new ArrayList<AbstractMap.SimpleEntry<Long, IInput.IInputEnum>>();
+		inputs = new ArrayList<SimpleEntry<Long, IInput.IInputEnum>>();
 	}
     
+	/**
+	 * Parses a file that specifies a number of timed inputs in the format
+	 * 
+	 * 1  COF
+	 * 9  PUB
+	 * 20 TEA
+	 * 
+	 * where the number of spaces between time and
+	 * input can be greater than one. The instructions
+	 * will be handled top to bottom. It is therefore
+	 * required, that the instructions are ordered after
+	 * time. Otherwise, the environment will end up in
+	 * a deadlock.
+	 * 
+	 * @param file Path to the file to parse.
+	 */
 	public void parse(String file) {
 		Scanner scanner = new Scanner(file);
+
+		String[] splitLine;
 		
 		while (scanner.hasNext()) {
-			String line = scanner.next();
-			String[] timeInput = line.split("\\s+");
-			
-			Long time = Long.parseLong(timeInput[0]);
-			IInput.IInputEnum input = IInput.IInputEnum.valueOf(timeInput[1]);
+			splitLine = scanner.next().split("\\s+");
+
+			long time = Long.parseLong(splitLine[0]);
+			IInput.IInputEnum input = IInput.IInputEnum.valueOf(splitLine[1]);
 			
 			inputs.add(new SimpleEntry<Long, IInput.IInputEnum>(time, input));
 		}
 	}
 	
+	/**
+	 * Executes the environment.
+	 */
 	public void run() {
 		
 		ControllerThread thread = new ControllerThread(controller);
@@ -63,7 +96,7 @@ public abstract class IEnvironment {
 		
 		try {
 			thread.join();
-		} catch (Exception e) {
+		} catch (InterruptedException e) {
 			System.err.print("Error: " + e.getMessage());
 		}
 	}
