17ddae9cdc2e24576f122820bf85ce7d87a8744e
Florian Biermann <fbie@itu.dk>
2012-11-05 18:37:42 +0100

    clean up log functionality a bit + indentation

--- dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/environment/Log.java
+++ dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/environment/Log.java
@@ -5,93 +5,89 @@ import java.io.FileWriter;
 
 public class Log {
 
-    // Singleton
-	
-    private Log(){   }
+	// Singleton
 
-    public enum LogWhat{ALL, IMPORTANT, NONE};
-    public enum Output{CONSOLE, FILE};
-
-    private LogWhat logwhat;
-    private Output output;
-
-    private static Log _instance = null;
-    
-    public static synchronized Log getInstance() {
-	if (_instance == null) {
-                _instance = new Log();
-                _instance.output = Output.FILE;
-                _instance.logwhat = LogWhat.ALL;
+	private Log() {
 	}
-	return _instance;
-    }
-    
-    // Log message to a file
-    
-    public static void log(String s) {
-	try {
-	    
-            // Create file if it does not exist and make sure to not overwrite data
 
-	    FileWriter fileWriter = new FileWriter("Log.txt", true);
-	    BufferedWriter outBuffer = new BufferedWriter(fileWriter);
-	    
-            
-	    String newLine = System.getProperty("line.separator");
-	    outBuffer.write(s+newLine);
-	    
-            
-	    outBuffer.close();
-            
-	} catch (Exception e) {
-	}
-    }
+	public enum LogWhat {
+		ALL, IMPORTANT, NONE
+	};
 
-    public void log(String s, LogWhat level, Output output) {
-	if (this.logwhat != LogWhat.NONE && level != null) {
-	    if (this.logwhat == LogWhat.ALL) {
-		
-	    }else if (level == LogWhat.IMPORTANT ) {
-		
-	    }else{
-		
-		return;
-	    }
-		
-	}
-	if (output == Output.CONSOLE || this.output == Output.CONSOLE) {
-		    logToConsole(s);
-		} else {
-		    logToFile(s);
+	public enum Output {
+		CONSOLE, FILE
+	};
+
+	private LogWhat logwhat;
+	private Output output;
+
+	private static Log _instance = null;
+
+	public static synchronized Log getInstance() {
+		if (_instance == null) {
+			_instance = new Log();
+			_instance.output = Output.CONSOLE;
+			_instance.logwhat = LogWhat.ALL;
 		}
-    }
-
-    public void setLogWhat(LogWhat level){
-	this.logwhat = level;
-    }
-
-    public void setOutput(Output output) {
-	this.output = output;
-    }
-
-    private void logToFile(String s) {
-	try {
-            
-            // Create file if it does not exist and make sure to not overwrite data
-            
-	    FileWriter fileWriter = new FileWriter("Log.txt", true);
-	    BufferedWriter outBuffer = new BufferedWriter(fileWriter);
-	    
-	    String newLine = System.getProperty("line.separator");
-	    outBuffer.write(s+newLine);
-	    
-	    outBuffer.close();
-	} catch (Exception e) {
+		return _instance;
 	}
-    }
 
-    private void logToConsole(String s){
-	System.out.println(s);
-    }
+	// Log message to a file
+
+	public static void log(String s) {
+		if (getInstance().output == Output.CONSOLE) {
+			getInstance().logToConsole(s);
+		} else {
+			getInstance().logToFile(s);
+		}
+	}
+
+	public void log(String s, LogWhat level, Output output) {
+		if (this.logwhat != LogWhat.NONE && level != null) {
+			if (this.logwhat == LogWhat.ALL) {
+				
+			} else if (level == LogWhat.IMPORTANT) {
+
+			} else {
+
+				return;
+			}
+
+		}
+		if (output == Output.CONSOLE || this.output == Output.CONSOLE) {
+			logToConsole(s);
+		} else {
+			logToFile(s);
+		}
+	}
+
+	public void setLogWhat(LogWhat level) {
+		this.logwhat = level;
+	}
+
+	public void setOutput(Output output) {
+		this.output = output;
+	}
+
+	private void logToFile(String s) {
+		try {
+
+			// Create file if it does not exist and make sure to not overwrite
+			// data
+
+			FileWriter fileWriter = new FileWriter("Log.txt", true);
+			BufferedWriter outBuffer = new BufferedWriter(fileWriter);
+
+			String newLine = System.getProperty("line.separator");
+			outBuffer.write(s + newLine);
+
+			outBuffer.close();
+		} catch (Exception e) {
+		}
+	}
+
+	private void logToConsole(String s) {
+		System.out.println(s);
+	}
 
 }
