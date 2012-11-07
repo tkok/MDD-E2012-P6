380821097d348de4429c8943c8256ea764bd18be
Florian Biermann <fbie@itu.dk>
2012-11-07 19:36:20 +0100

    add ability to log to csv files

--- dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/environment/QuickLog.java
+++ dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/environment/QuickLog.java
@@ -1,28 +1,103 @@
 package dk.itu.ecdar.text.generator.environment;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class QuickLog {
 
-	private static int logLevel = 0;
+	private static QuickLog _instance = null;
+	
+	private int logLevel;
+	private volatile int counter;
+	
+	private boolean console ;
+	private String destination;
+	
+	private QuickLog() {
+		logLevel = 0;
+		counter = 0;
+		
+		console = true;
+		
+		DateFormat format = new SimpleDateFormat("yyyy-mm-dd-hh-mm-ss");
+		Date date = new Date();
+		
+		destination = "log-" + format.format(date) + ".csv";
+	}
+	
+	private static QuickLog getInstance() {
+		if (_instance == null) {
+			_instance = new QuickLog();
+		}
+		return _instance;
+	}
+	
+	public static void print(String message) {
+		print(message, 0);
+	}
+	
+	public static void print(String message, int level) {
+		if (level <= getInstance().logLevel)
+			System.err.println("[QuickLog] " + message);
+	}
 	
 	public static void setLogLevel(int level) {
-		logLevel = level;
+		getInstance().logLevel = level;
 	}
 	
-	public static void log(String message) {
-		log(message, 0);
+	public static void logToConsole() {
+		getInstance().console = true;
 	}
 	
-	public static void log(String message, int level) {
-		if (level <= logLevel)
-			System.out.println(message);
+	public static void logToFile() {
+		getInstance().console = false;
 	}
 	
-	public static void error(String message) {
-		error(message, 0);
-	}
-	public static void error(String message, int level) {
-		if (level <= logLevel)
-			System.err.println(message);
+	public static void setDestination(String filepath) {
+		getInstance().destination = filepath;
 	}
 	
+	public static void log(String source, long time, String message) {
+		log(source, time, message, 0);
+	}
+	
+	public static synchronized void log(String source, long time, String message, int level) {
+		getInstance().internal_log(source, time, message, level);
+	}
+	
+	private void internal_log(String source, long time, String message, int level) {
+		if (level <= logLevel) {
+			if (console)
+				internal_logToConsole(source, time, message);
+			else
+				internal_logToFile(source, time, message);
+		}
+	}
+	
+	private synchronized void internal_logToConsole(String source, long time, String message) {
+		System.err.println("[" + String.valueOf(++counter) + "] ["
+				+ source + "] [T=" + String.valueOf(time) + "] " + message);
+	}
+	
+	private synchronized void internal_logToFile(String source, long time, String message) {
+		String msg = String.valueOf(++counter) + ","
+				+ source + ","
+				+ String.valueOf(time) + ","
+				+ message;
+		
+		try {
+			FileWriter fileWriter = new FileWriter(destination, true);
+			BufferedWriter outBuffer = new BufferedWriter(fileWriter);
+
+			String newLine = System.getProperty("line.separator");
+			outBuffer.write(msg + newLine);
+
+			outBuffer.close();
+		} catch (Exception e) {
+			System.err.println("ERROR: Could not write to file " + destination);
+		}
+	}
 }
