83237ea98b3045a524481aa02ef981965dc6fef6
Nicolas Wainach <nw@netstarter.dk>
2012-11-07 17:55:51 +0100

    Further refactoring
    Further refactoring

--- dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/generators/SayHello.java
+++ dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/generators/SayHello.java
@@ -2,6 +2,9 @@
 // Hint: you can use this component to debug your workflows (I find using assertions easier, though) 
 package dk.itu.ecdar.text.generator.generators;
 
+import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowComponent;
+import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowContext;
+
 public class SayHello implements IWorkflowComponent {
 
 	private String message = "Hello World!";
