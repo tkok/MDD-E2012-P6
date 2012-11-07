c5c595f5da9b24c653096dddf26f9c410b971440
Florian Biermann <fbie@itu.dk>
2012-11-07 20:19:05 +0100

    change example to original configuration

--- dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/mockup/example/university/UniversityEnvironment.java
+++ dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/mockup/example/university/UniversityEnvironment.java
@@ -7,8 +7,7 @@ public class UniversityEnvironment extends IEnvironment {
 
 	@Override
 	public void generateController() {
-		//controller = UniversityController.system_Machine_Researcher_University_HalfUni1_HalfUni2_Spec();
-		controller = UniversityController.system_Machine_Researcher_HalfUni1_HalfUni2_Spec();
+		controller = UniversityController.system_Machine_Researcher_University_HalfUni1_HalfUni2_Spec();
 	}
 
 	public static void main(String[] args) {
