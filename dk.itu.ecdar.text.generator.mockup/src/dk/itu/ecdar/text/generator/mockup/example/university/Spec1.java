6161c802470f8ab29c12211689e61009a215cb5f
lauge <laugeg@gmail.com>
2012-11-07 17:46:17 +0100

    Added University, University2 and Spec1 to mockup
    University2 and Spec1 doesn't have location names. So implemented
    University2 and Spec1 with the same location names as University and
    Spec respectively.

--- dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/mockup/example/university/Spec1.java
+++ dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/mockup/example/university/Spec1.java
@@ -1,7 +1,148 @@
 package dk.itu.ecdar.text.generator.mockup.example.university;
 
+import dk.itu.ecdar.text.generator.framework.IEdgeControllable;
+import dk.itu.ecdar.text.generator.framework.IEdgeUncontrollable;
+import dk.itu.ecdar.text.generator.framework.ILocation;
 import dk.itu.ecdar.text.generator.framework.ITIOA;
 
-public class Spec1 extends ITIOA {
+public class Spec extends ITIOA {
 
+	// Controllable edges
+	class Start_GRANT_Grant extends IEdgeControllable {
+
+		public Start_GRANT_Grant() {
+			super(start, grant, "grant");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return from.checkInvariant(time);
+		}
+
+		@Override
+		public void onTraverse() {
+			resetTime();
+			
+		}
+		
+	}
+	
+	class Grant_GRANT_Grant extends IEdgeControllable {
+
+		public Grant_GRANT_Grant() {
+			super(grant, grant, "grant");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return from.checkInvariant(time) && true;
+		}
+
+		@Override
+		public void onTraverse() {
+			
+		}
+		
+	}
+		
+	// Uncontrollable edges
+	class Grant_PATENT_Start extends IEdgeUncontrollable {
+
+		public Grant_PATENT_Start() {
+			super(grant, start, "patent");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return from.checkInvariant(time) && true;
+		}
+
+		@Override
+		public void onTraverse() {
+			resetTime();
+			
+		}
+		
+	}
+		
+	// Locations
+	class Start extends ILocation {
+
+		public Start(Spec parent) {
+			super("start", parent);
+		}
+
+		@Override
+		public void setupEdges() {
+			outputEdges = new IEdgeUncontrollable[]{};
+			
+			inputEdges = new IEdgeControllable[]{
+					new Start_GRANT_Grant()
+			};
+		}
+
+		@Override
+		public boolean checkInvariant(long time) {
+			return true;
+		}
+
+		@Override
+		public boolean isPreemptive() {
+			return false;
+		}
+
+		@Override
+		public void task() {
+			
+		}
+		
+	}
+		
+	class Grant extends ILocation {
+
+		public Grant(Spec parent) {
+			super("grant", parent);
+		}
+
+		@Override
+		public void setupEdges() {
+			outputEdges = new IEdgeUncontrollable[]{
+					new Grant_PATENT_Start()
+			};
+			
+			inputEdges = new IEdgeControllable[]{
+					new Grant_GRANT_Grant()
+			};
+		}
+
+		@Override
+		public boolean checkInvariant(long time) {
+			return time <= 20;
+		}
+
+		@Override
+		public boolean isPreemptive() {
+			return false;
+		}
+
+		@Override
+		public void task() {
+			
+		}
+		
+	}
+	
+	ILocation start, grant;
+	
+	public Spec() {
+		super();
+		
+		start = new Start(this);
+		grant = new Grant(this);
+		
+		start.setupEdges();
+		grant.setupEdges();
+		
+		current = start;
+	}
 }
--- dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/mockup/example/university/University.java
+++ dk.itu.ecdar.text.generator.mockup/src/dk/itu/ecdar/text/generator/mockup/example/university/University.java
@@ -1,7 +1,314 @@
 package dk.itu.ecdar.text.generator.mockup.example.university;
 
+import dk.itu.ecdar.text.generator.framework.IEdgeControllable;
+import dk.itu.ecdar.text.generator.framework.IEdgeUncontrollable;
+import dk.itu.ecdar.text.generator.framework.ILocation;
 import dk.itu.ecdar.text.generator.framework.ITIOA;
 
-public class University extends ITIOA {
+public class Spec extends ITIOA {
 
+	// Controllable edges
+	class A_GRANT_B extends IEdgeControllable {
+
+		public A_GRANT_B() {
+			super(A, B, "grant");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return from.checkInvariant(time);
+		}
+
+		@Override
+		public void onTraverse() {
+			resetTime();
+		}
+		
+	}
+		
+	class B_GRANT_B extends IEdgeControllable {
+
+		public B_GRANT_B() {
+			super(B, B, "grant");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return from.checkInvariant(time);
+		}
+
+		@Override
+		public void onTraverse() {
+			
+		}
+		
+	}
+
+	class B_PUB_B extends IEdgeControllable {
+
+		public B_PUB_B() {
+			super(B, B, "grant");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return from.checkInvariant(time);
+		}
+
+		@Override
+		public void onTraverse() {
+			
+		}
+		
+	}
+	
+	class C_GRANT_C extends IEdgeControllable {
+		
+		public C_GRANT_C() {
+			super(C, C, "grant");
+		}
+		
+		@Override
+		public boolean checkGuard(long time) {
+			return from.checkInvariant(time);
+		}
+		
+		@Override
+		public void onTraverse() {}
+	}
+	
+	class C_PUB_D extends IEdgeControllable {
+		
+		public C_PUB_D() {
+			super(C, D, "pub");
+		}
+		
+		@Override
+		public boolean checkGuard(long time) {
+			return from.checkInvariant(time);
+		}
+		
+		@Override
+		public void onTraverse(){
+			resetTime();
+		}
+	}
+	
+	class D_PUB_D extends IEdgeControllable {
+		
+		
+		public D_PUB_D() {
+			super(D, D, "pub");
+		}
+		
+		@Override
+		public boolean checkGuard(long time) {
+			return from.checkInvariant(time);
+		}
+		
+		@Override
+		public void onTraverse(){
+		}
+	}
+
+	class D_GRANT_D extends IEdgeControllable {
+		
+		
+		public D_GRANT_D() {
+			super(D, D, "grant");
+		}
+		
+		@Override
+		public boolean checkGuard(long time) {
+			return from.checkInvariant(time);
+		}
+		
+		@Override
+		public void onTraverse(){
+		}
+	}
+
+	// Uncontrollable edges
+	class B_COIN_C extends IEdgeUncontrollable {
+		public B_COIN_C() {
+			super(B, C, "coin");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return from.checkInvariant(time);
+		}
+
+		@Override
+		public void onTraverse() {
+		}
+	}
+	
+	class D_PATENT_A extends IEdgeUncontrollable {
+		public D_PATENT_A() {
+			super(D, A, "patent");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return from.checkInvariant(time);
+		}
+
+		@Override
+		public void onTraverse() {
+		}
+	}
+	
+	// Locations
+	class A extends ILocation {
+
+		public A(Spec parent) {
+			super("A", parent);
+		}
+
+		@Override
+		public void setupEdges() {
+			outputEdges = new IEdgeUncontrollable[]{};
+			
+			inputEdges = new IEdgeControllable[]{
+					new A_GRANT_B()
+			};
+		}
+
+		@Override
+		public boolean checkInvariant(long time) {
+			return true;
+		}
+
+		@Override
+		public boolean isPreemptive() {
+			return false;
+		}
+
+		@Override
+		public void task() {
+			
+		}
+		
+	}
+	
+	class B extends ILocation {
+
+		public B(Spec parent) {
+			super("B", parent);
+		}
+
+		@Override
+		public void setupEdges() {
+			outputEdges = new IEdgeUncontrollable[]{
+					new B_COIN_C()
+			};
+			
+			inputEdges = new IEdgeControllable[]{
+					new B_GRANT_B(),
+					new B_PUB_B()
+			};			
+		}
+
+		@Override
+		public boolean checkInvariant(long time) {
+			return time <= 2;
+		}
+
+		@Override
+		public boolean isPreemptive() {
+			return false;
+		}
+
+		@Override
+		public void task() {
+		}
+		
+	}
+	
+	class C extends ILocation {
+
+		public C(Spec parent) {
+			super("C", parent);
+		}
+
+		@Override
+		public void setupEdges() {
+			outputEdges = new IEdgeUncontrollable[]{
+			};
+			
+			inputEdges = new IEdgeControllable[]{
+					new C_GRANT_C(),
+					new C_PUB_D()
+			};
+		}
+
+		@Override
+		public boolean checkInvariant(long time) {
+		}
+
+		@Override
+		public boolean isPreemptive() {
+			return false;
+		}
+
+		@Override
+		public void task() {
+			
+		}
+		
+	}
+
+	class D extends ILocation {
+
+		public D(Spec parent) {
+			super("D", parent);
+		}
+
+		@Override
+		public void setupEdges() {
+			outputEdges = new IEdgeUncontrollable[]{
+					new D_PATENT_A()
+			};
+			
+			inputEdges = new IEdgeControllable[]{
+					new D_GRANT_D(),
+					new D_PUB_D()
+			};
+		}
+
+		@Override
+		public boolean checkInvariant(long time) {
+			return time <= 2;
+		}
+
+		@Override
+		public boolean isPreemptive() {
+			return false;
+		}
+
+		@Override
+		public void task() {
+			
+		}
+		
+	}
+
+	ILocation A, B, C, D;
+	
+	public University() {
+		super();
+		
+		A = new A(this);
+		B = new B(this);
+		C = new C(this);
+		D = new D(this);
+		
+		A.setupEdges();
+		B.setupEdges();
+		C.setupEdges();
+		D.setupEdges();
+		
+		current = A;
+	}
 }
