d49d3e9c82943cd39b19cdf1ad770628bca0f30f
Thomas Kokholm <tkok@itu.dk>
2012-11-23 13:43:58 +0100

    changes to analysis

--- Analyse/src/analyse/Analyse.java
+++ Analyse/src/analyse/Analyse.java
@@ -18,44 +18,40 @@ public class Analyse {
     /**
      * @param args the command line arguments
      */
-    
     public static void setFlag(String flag) {
         int[] orderArray;
         orderArray = new int[3];
         //GRANT = 0
         //COIN = 1
         //TA = 2
-        
-        if("GRANT".equals(flag)) {
+
+        if ("GRANT".equals(flag)) {
             orderArray[0] = 1;
-        }
-        else if("COIN".equals(flag)) {
-            if(orderArray[0] != 0) { // if GRANT exist
+        } else if ("COIN".equals(flag)) {
+            if (orderArray[0] != 0) { // if GRANT exist
                 orderArray[1] = 1;
-            }
-            else {
+            } else {
                 System.out.println("==================================\nERROR: Signaling " + flag + " before GRANT \n==================================");
             }
-        }
-        else if("TEA".equals(flag)) {
-            
-            if(orderArray[1] != 0) { // if COIN exist
+        } else if ("TEA".equals(flag)) {
+
+            if (orderArray[1] != 0) { // if COIN exist
                 orderArray[2] = 1;
-            }
-            else {
+            } else {
                 System.out.println("================================\nERROR: Signaling " + flag + " before COIN \n================================");
             }
         }
     }
+
     public void chechOrder() {
-        
     }
+
     public static void main(String[] args) {
         // TODO code application logic here
 
         try {
             // Open file to FileInputStram
-            FileInputStream fstream = new FileInputStream("log1.csv");
+            FileInputStream fstream = new FileInputStream("log2.csv");
             // Get the object of DataInputStream
             DataInputStream in = new DataInputStream(fstream);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
@@ -63,22 +59,20 @@ public class Analyse {
             //Read File Line By Line
 
             for (int i = 0; i < br.readLine().length(); i++) {
-                if(br.readLine() != null) {
+                if (br.readLine() != null) {
                     strLine = br.readLine();
                     System.out.println(strLine);
-                    if(strLine.matches(".*\\bSignaling grant\\b.*")) {
+                    if (strLine.matches(".*\\bSignaling grant\\b.*")) {
                         //System.out.println(">> GRANT!");
                         setFlag("GRANT");
-                    }
-                    else if(strLine.matches(".*\\bcoin\\b.*")) {
+                    } else if (strLine.matches(".*\\bSignaling coin\\b.*")) {
                         //System.out.println(">> COIN!");
                         setFlag("COIN");
-                    }
-                    else if(strLine.matches(".*\\bSignaling tea\\b.*")) {
+                    } else if (strLine.matches(".*\\bSignaling tea\\b.*")) {
                         //System.out.println(">> TEA!");
                         setFlag("TEA");
                     }
-                    
+
                 }
             }
 
