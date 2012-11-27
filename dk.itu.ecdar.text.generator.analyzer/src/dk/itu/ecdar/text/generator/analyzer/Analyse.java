5ebb0b5b8f0ac7b90690c48b3c03776c6e737ca1
Florian Biermann <fbie@itu.dk>
2012-11-27 14:15:39 +0100

    rename Analyse to go with the flow
    Also updated Netbeans project internals to keep everything running for everyone.

--- dk.itu.ecdar.text.generator.analyzer/src/dk/itu/ecdar/text/generator/analyzer/Analyse.java
+++ dk.itu.ecdar.text.generator.analyzer/src/dk/itu/ecdar/text/generator/analyzer/Analyse.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package analyse;
+package dk.itu.ecdar.text.generator.analyzer;
 
 import java.io.File;
 import java.util.Scanner;
