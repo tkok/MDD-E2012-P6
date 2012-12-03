9ddfcacc00494947c47cbee938f1483dec0d7b52
Nicolas Wainach <nw@netstarter.dk>
2012-12-03 13:05:27 +0100

    Task & Controller done
    Task & Controller done

--- dk.itu.ecdar.text.generator/model-gen/Controller.java
+++ dk.itu.ecdar.text.generator/model-gen/Controller.java
@@ -7,22 +7,131 @@ public class Controller extends IController {
 
 	public Controller() {
          super(new ITIOA[]{
-         	
-         		 //Insert new + specnames() here
-                 
+				new Machine(),
+				new University2(),
+				new Spec(),
+				new University(),
+				new MachineImpl(),
+				new MachineRef(),
+				new ResearcherImpl(),
+				new Researcher()
                  });
          IController.controllerInstance = this;
      }
-     
+   
     // Define tasks for every spec->location
      
-    public static void classnameLocationname() {
+				
+	public static void MachineServing() {
         
-        System.out.print("testing task");
-    
+        // define task here
+   
     }
-     
-     
-}
+    
+				
+	public static void University2id2() {
+        
+        // define task here
+   
+    }
+	public static void University2Inconsistent() {
+        
+        // define task here
+   
+    }
+	public static void University2id4() {
+        
+        // define task here
+   
+    }
+	public static void University2Universal() {
+        
+        // define task here
+   
+    }
+	public static void University2id6() {
+        
+        // define task here
+   
+    }
+    
+				
+	public static void SpecEnd() {
+        
+        // define task here
+   
+    }
+	public static void SpecGrant() {
+        
+        // define task here
+   
+    }
+    
+				
+	public static void UniversityD() {
+        
+        // define task here
+   
+    }
+	public static void UniversityC() {
+        
+        // define task here
+   
+    }
+	public static void UniversityB() {
+        
+        // define task here
+   
+    }
+    
+				
+	public static void MachineImplServing() {
+        
+        // define task here
+   
+    }
+    
+				
+	public static void MachineRefServing() {
+        
+        // define task here
+   
+    }
+    
+				
+	public static void ResearcherImpllu() {
+        
+        // define task here
+   
+    }
+	public static void ResearcherImplid21() {
+        
+        // define task here
+   
+    }
+	public static void ResearcherImplid22() {
+        
+        // define task here
+   
+    }
+    
+				
+	public static void ResearcherUNIV() {
+        
+        // define task here
+   
+    }
+	public static void ResearcherT() {
+        
+        // define task here
+   
+    }
+	public static void ResearcherC() {
+        
+        // define task here
+   
+    }
+    
 
+}
 
