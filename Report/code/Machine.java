186d38ba8cd15f60fc33aed7609d8ee99d8cdcdc
Thomas Kokholm <tkok@itu.dk>
2012-12-12 13:05:21 +0100

    Commited the latest version of the report from sharelatex
    Just in case sharelatex crashes ;-)

---
+++ Report/code/Machine.java
@@ -0,0 +1,184 @@
+package dk.itu.ecdar.text.generator.mockup.university;
+
+import dk.itu.ecdar.text.generator.framework.IInputEdge;
+import dk.itu.ecdar.text.generator.framework.IOutputEdge;
+import dk.itu.ecdar.text.generator.framework.ILocation;
+import dk.itu.ecdar.text.generator.framework.ITIOA;
+
+public class Machine extends ITIOA {
+	
+	// Controllable edges
+	class Idle_COIN_Serving extends IInputEdge {
+
+		public Idle_COIN_Serving() {
+			super(serving, "coin");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return to.checkInvariant(time) && true;
+		}
+
+		@Override
+		public void onTraverse() {
+			resetTime();
+			
+		}
+	}
+	
+	class Serving_COIN_Serving extends IInputEdge {
+
+		public Serving_COIN_Serving() {
+			super(serving, "coin");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return to.checkInvariant(time) && true;
+		}
+
+		@Override
+		public void onTraverse() {
+			
+		}
+	}
+	
+	// Uncontrollable edges
+	class Idle_TEA_Idle extends IOutputEdge {
+
+		public Idle_TEA_Idle() {
+			super(idle, "tea");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return to.checkInvariant(time) && time >= 2;
+		}
+
+		@Override
+		public void onTraverse() {
+			
+		}
+	}
+	
+	class Serving_TEA_Idle extends IOutputEdge {
+
+		public Serving_TEA_Idle() {
+			super(idle, "tea");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return to.checkInvariant(time) && true;
+		}
+
+		@Override
+		public void onTraverse() {
+			
+		}
+		
+	}
+	
+	class Serving_COF_Idle extends IOutputEdge {
+
+		public Serving_COF_Idle() {
+			super(idle, "cof");
+		}
+
+		@Override
+		public boolean checkGuard(long time) {
+			return to.checkInvariant(time) && time >= 4;
+		}
+
+		@Override
+		public void onTraverse() {
+			
+		}
+		
+	}
+	
+	// Locations
+	class Idle extends ILocation {
+
+		public Idle(Machine parent) {
+			super("idle", parent);
+		}
+		
+		@Override
+		public void setupEdges() {
+			outputEdges = new IOutputEdge[]{
+					new Idle_TEA_Idle()
+			};
+			
+			inputEdges = new IInputEdge[]{
+					new Idle_COIN_Serving()
+			};
+		}
+
+		@Override
+		public boolean checkInvariant(long time) {
+			return true;
+		}
+
+		@Override
+		public boolean isPreemptive() {
+			return false;
+		}
+
+		@Override
+		public void task() {
+			
+		}
+	}
+	
+	class Serving extends ILocation {
+		
+		public Serving(Machine parent) {
+			super("serving", parent);
+		}
+		
+		@Override
+		public void setupEdges() {
+
+			outputEdges = new IOutputEdge[]{
+					new Serving_TEA_Idle(),
+					new Serving_COF_Idle()
+			};
+			
+			inputEdges = new IInputEdge[]{
+					new Serving_COIN_Serving()
+			};
+			
+		}
+
+		@Override
+		public boolean checkInvariant(long time) {
+			return time <= 6;
+		}
+
+		@Override
+		public boolean isPreemptive() {
+			return false;
+		}
+
+		@Override
+		public void task() {
+			
+		}		
+	}
+	
+	ILocation idle, serving;
+	
+	public Machine() {
+		super();
+		
+		idle = new Idle(this);
+		serving = new Serving(this);
+		
+		idle.setupEdges();
+		serving.setupEdges();
+		
+		current = idle;
+	}
+		
+}
---
+++ Report/code/UniversityController.java
@@ -0,0 +1,63 @@
+package dk.itu.ecdar.text.generator.mockup.university;
+
+import dk.itu.ecdar.text.generator.framework.IController;
+import dk.itu.ecdar.text.generator.framework.ITIOA;
+
+public class UniversityController extends IController {
+
+	public UniversityController(ITIOA[] automata) {
+		super(automata);
+		IController.controllerInstance = this;
+	}
+	
+	/* These static factories should normally not be here but
+	 * instead the specification should make the generator place
+	 * the correct configuration in the constructor above,
+	 * calling super() with the right array of automata. However,
+	 * for testing it is more convenient to have it like this so
+	 * we can switch quickly.
+	 */
+	public static UniversityController
+	system_Machine_Researcher_University_HalfUni1_HalfUni2_Spec() {
+		return new UniversityController(new ITIOA[]{new Machine(),
+				new Researcher(),
+				new University(),
+				new HalfUni1(),
+				new HalfUni2()});
+	}
+	
+	public static UniversityController
+	system_Machine_Researcher_HalfUni1_HalfUni2_Spec() {
+		return new UniversityController(new ITIOA[]{new Machine(),
+				new Researcher(),
+				new HalfUni1(),
+				new HalfUni2()});
+	}
+	
+	public static UniversityController
+	system_Machine_Researcher_University2_Spec() {
+		return new UniversityController(new ITIOA[]{new Machine(),
+				new Researcher(),
+				new University2()});
+	}
+	
+	public static UniversityController
+	system_Machine_Researcher_University_Spec() {
+		return new UniversityController(new ITIOA[]{new Machine(),
+				new Researcher(),
+				new University()
+});
+	}
+	
+	public static UniversityController
+	system_Machine_Researcher_University() {
+		return new UniversityController(new ITIOA[]{new Machine(),
+				new Researcher(),
+				new University()});
+	}
+	
+	public static UniversityController
+	system_Spec() {
+		return new UniversityController(new ITIOA[]{new Spec()});
+	}
+}
